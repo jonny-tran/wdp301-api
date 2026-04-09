00c7de6fc67120a36365e1ea9a29f23baffb62b5
DatTTSE172775 <datttse172775@fpt.edu.vn>
2026-04-09 16:34:20 +0700

    fix bug deployment

--- src/module/shipment/shipment.module.ts
+++ src/module/shipment/shipment.module.ts
@@ -2,12 +2,18 @@ import { forwardRef, Module } from '@nestjs/common';
 import { DatabaseModule } from '../../database/database.module';
 import { ClaimModule } from '../claim/claim.module';
 import { InventoryModule } from '../inventory/inventory.module';
+import { SystemConfigModule } from '../system-config/system-config.module';
 import { ShipmentController } from './shipment.controller';
 import { ShipmentRepository } from './shipment.repository';
 import { ShipmentService } from './shipment.service';
 
 @Module({
-  imports: [DatabaseModule, InventoryModule, forwardRef(() => ClaimModule)],
+  imports: [
+    DatabaseModule,
+    InventoryModule,
+    SystemConfigModule,
+    forwardRef(() => ClaimModule),
+  ],
   controllers: [ShipmentController],
   providers: [ShipmentRepository, ShipmentService],
   exports: [ShipmentService, ShipmentRepository],
